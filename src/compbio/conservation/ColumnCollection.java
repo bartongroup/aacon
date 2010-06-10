eb481d69979d5a3c5be9a4b17d6cc3af98cdf679
agolicz <agolicz@e3abac25-378b-4346-85de-24260fe3988d>
2010-06-10 16:46:17 +0000

    Modified ColumnCollection. Fits the use of Map.

--- src/compbio/conservation/ColumnCollection.java
+++ src/compbio/conservation/ColumnCollection.java
@@ -1,31 +1,53 @@
 package compbio.conservation;
 
+import java.util.*;
+
 public class ColumnCollection {
 
-private AminoAcidColumn[] col;
+private Column[] col;
 
 public ColumnCollection(AminoAcidMatrix m) {
 	
-	col = new AminoAcidColumn[m.numberOfColumns()];
+	if (m == null) {
+		
+		throw new IllegalArgumentException("m must not be null");
+	}
+	
+	assert m.numberOfColumns() != 0 : "Sth wrong with the matrix, not null but doesn't have any residues";
+	
+	col = new Column[m.numberOfColumns()];
 	
 	for (int i = 0; i < m.numberOfColumns(); i++) {
 		
-		col[i] = new AminoAcidColumn(m, i);
+		col[i] = new Column(m, i);
 		
 	}
 	
 }	
 	
-public AminoAcidColumn[] getColumnCollection () {
+Column[] getColumnCollection () {
 	
 	return col;
 }
 
-public int collectionLength() {
+int collectionLength() {
 	
 	int len = col.length;
 	
 	return len;
 }
+// this method calculates the overall amino acid frequency over the column window present
+// still to be written
 
+Map<Character,Integer> TotalAcids() {
+	
+	Map<Character,Integer>  freq = new HashMap<Character,Integer>();
+	
+	
+	return freq;
+	
 }
+}
+
+
+
