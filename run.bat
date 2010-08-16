d2e49c96f55e4e6f4e4cc1b9f32179c867253a41
pvtroshin <pvtroshin@e3abac25-378b-4346-85de-24260fe3988d>
2010-08-16 15:03:18 +0000

    further get rid of unused methods, renaming used to more appropriate names

---
+++ run.bat
@@ -0,0 +1,20 @@
+java -jar  compbio-conservation-1.0.jar -i=test/data/1000x3000DNA.aln.fa  -m=KABAT -o=kabat.txt -f=RESULT_NO_ALIGNMENT -d=kabat.d
+java -jar  compbio-conservation-1.0.jar -i=test/data/large.aln.fa -m=JORES -o=jores.txt -f=RESULT_NO_ALIGNMENT -d=jores.d
+java -jar  compbio-conservation-1.0.jar -i=test/data/TO1296.fasta.align  -m=SCHNEIDER -o=schneider.txt -f=RESULT_NO_ALIGNMENT -d=schneider.d
+java -jar  compbio-conservation-1.0.jar -i=test/data/TO1296.fasta.align -m=SHENKIN -o=shenkin.txt -f=RESULT_NO_ALIGNMENT
+java -jar  compbio-conservation-1.0.jar -i=test/data/TO1296.fasta.align -m=GERSTEIN -o=gerstein.txt -f=RESULT_NO_ALIGNMENT
+java -jar  compbio-conservation-1.0.jar -i=test/data/TO1296.fasta.align -m=TAYLOR_GAPS -o=taylor.txt -f=RESULT_NO_ALIGNMENT
+java -jar  compbio-conservation-1.0.jar -i=test/data/TO1296.fasta.align -m=TAYLOR_NO_GAPS -o=taylor_nogaps.txt -f=RESULT_NO_ALIGNMENT
+java -jar  compbio-conservation-1.0.jar -i=test/data/TO1296.fasta.align -m=ZVELIBIL -o=zvelibil.txt -f=RESULT_NO_ALIGNMENT
+
+java -jar  compbio-conservation-1.0.jar -i=test/data/TO1296.fasta.align -m=KARLIN -o=kalinin.txt -f=RESULT_NO_ALIGNMENT
+java -jar  compbio-conservation-1.0.jar -i=test/data/TO1296.fasta.align -m=ARMON -o=armon.txt -f=RESULT_NO_ALIGNMENT
+java -jar  compbio-conservation-1.0.jar -i=test/data/TO1296.fasta.align -m=THOMPSON -o=thompson.txt -f=RESULT_NO_ALIGNMENT
+java -jar  compbio-conservation-1.0.jar -i=test/data/TO1296.fasta.align -m=NOT_LANCET -o=not_lancet.txt -f=RESULT_NO_ALIGNMENT
+java -jar  compbio-conservation-1.0.jar -i=test/data/TO1296.fasta.align -m=MIRNY -o=mirny.txt -f=RESULT_NO_ALIGNMENT
+java -jar  compbio-conservation-1.0.jar -i=test/data/TO1296.fasta.align -m=WILLIAMSON -o=williamson.txt -f=RESULT_NO_ALIGNMENT
+java -jar  compbio-conservation-1.0.jar -i=test/data/TO1296.fasta.align -m=LANDGRAF -o=landgraf.txt -f=RESULT_NO_ALIGNMENT
+java -jar  compbio-conservation-1.0.jar -i=test/data/TO1296.fasta.align -m=SANDER -o=sander.txt -f=RESULT_NO_ALIGNMENT
+java -jar  compbio-conservation-1.0.jar -i=test/data/TO1296.fasta.align -m=VALDAR -o=valdar.txt -f=RESULT_NO_ALIGNMENT
+java -jar  compbio-conservation-1.0.jar -i=test/data/TO1296.fasta.align -m=SMERFS -o=smerfs.txt -f=RESULT_NO_ALIGNMENT -d=smerfs.d -s=5,MID_SCORE,0.1
+      
