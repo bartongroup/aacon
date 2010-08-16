b03f3e8d42a970057bcd53e9114e60f65967af40
pvtroshin <pvtroshin@e3abac25-378b-4346-85de-24260fe3988d>
2010-08-16 15:04:31 +0000

    further get rid of unused methods

--- test/src/compbio/util/SequenceUtilTester.java
+++ test/src/compbio/util/SequenceUtilTester.java
@@ -6,14 +6,11 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.List;
 
-import org.apache.log4j.Logger;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class SequenceUtilTester {
-
-    public final static Logger log = Logger.getLogger(SequenceUtilTester.class);
 
     public final static String DATA_PATH = "test/data";
 
@@ -21,19 +18,14 @@ public class SequenceUtilTester {
 
     @BeforeClass
     public void setup() throws FileNotFoundException, IOException {
-	fastlist = SequenceUtil.readFasta(new FileInputStream(new File(DATA_PATH
-		+ "/" + "TO1296.fasta.align")));
+	fastlist = SequenceUtil.readFasta(new FileInputStream(new File(
+		DATA_PATH + "/" + "TO1296.fasta.align")));
     }
 
     @Test(invocationCount = 3, threadPoolSize = 3)
     public void testLoadFile() {
 	Assert.assertNotNull(fastlist);
-	log.trace("Trace" + fastlist.size());
-	log.debug("Debug" + fastlist.size());
-	log.warn("Warn" + fastlist.size());
-	log.info("Info" + fastlist.size());
-	log.error("Error" + fastlist.size());
-	log.fatal("Fatal" + fastlist.size());
+
 	Assert.assertEquals(fastlist.size(), 179);
 	int length = 0;
 	for (FastaSequence fseq : fastlist) {
